dca1c583e3b318552296a6c4b44064fdc4502d07
Rafael Gouveia <102384501+RG-202100721@users.noreply.github.com>
2022-04-08 18:47:24 +0100

    Updated connection.js

--- scripts/connection.js
+++ scripts/connection.js
@@ -1,15 +1,28 @@
 var fs = require("fs");
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    //credenciais do servidor mysql
-    host: "remotemysql.com",
-    port: 3306,
-    database: "XuJ9K1LH7W",
-    user: "XuJ9K1LH7W",
-    password: "iXpUAq7fVx",
-    multipleStatements: true
-});
+var type = "local";
+if (type == "local") {
+    var con = mysql.createConnection({
+        //credenciais do servidor mysql
+        host: "localhost",
+        user: "root",
+        database: "prog_web_projeto",
+        password: "",
+        multipleStatements: true
+    });
+}
+else {
+    var con = mysql.createConnection({
+        //credenciais do servidor mysql
+        host: "remotemysql.com",
+        port: 3306,
+        database: "XuJ9K1LH7W",
+        user: "XuJ9K1LH7W",
+        password: "iXpUAq7fVx",
+        multipleStatements: true
+    });
+}
 
 //criação da conexão ao servidor mysql
 exports.start = function () {
@@ -32,13 +45,11 @@ exports.create = function () {
 exports.query = function (sql, callback) {
     con.query(sql, function (err, result) {
         if (err) throw err;
-        if (JSON.stringify(sql).startsWith("SELECT", 1)) 
-        {
+        if (JSON.stringify(sql).includes("SELECT")) {
             console.log('Database queried!\nQuery: ' + sql);
             callback(null, result);
         }
-        else 
-        {
+        else {
             console.log('Database updated!\nQuery: ' + sql);
             callback(null, null);
         }
