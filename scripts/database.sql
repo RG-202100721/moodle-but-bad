514223ecdd39aa6b94e2c2c443c32dca6472ee96
Rafael Gouveia <102384501+RG-202100721@users.noreply.github.com>
2022-04-22 20:08:20 +0100

    Updated server.js/database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS Inscricao (
 DROP TABLE IF EXISTS Revisao;
 CREATE TABLE IF NOT EXISTS Revisao (
   ID int NOT NULL AUTO_INCREMENT, 
-  Dia_Revisao date,  
+  Dia_Revisao varchar(10),  
   IDDisciplina int NOT NULL, 
   IDAluno int NOT NULL,
   Nota_Antes decimal(10,2),
@@ -40,7 +40,7 @@ DROP TABLE IF EXISTS Aluno;
 CREATE TABLE IF NOT EXISTS Aluno (
   ID int NOT NULL AUTO_INCREMENT, 
   Nome varchar(255), 
-  Data_Nascimento date, 
+  Data_Nascimento varchar(10), 
   Genero char,
   Email varchar(50),
   URLFoto varchar(255),
@@ -70,16 +70,16 @@ INSERT INTO Turma (Nome, Ano, Nome_Responsavel, Email_Responsavel, Curso) VALUES
 ('13-AC', 13, 'Ludmila Feitosa', 'aegerter@pyhaihyrt.com', 'Automação e Computadores');
 
 INSERT INTO Aluno (Nome, Data_Nascimento, Genero, Email, URLFoto) VALUES
-('Garcez Milheiriço', str_to_date('2002.03.05', '%Y.%m.%d'), 'F', 'cusaum@eetieg.com', 'https://picsum.photos/seed/1/500'),
-('Georgi Bogado', str_to_date('2001.04.10', '%Y.%m.%d'), 'M', 'kristian4811@filevino.com', 'https://picsum.photos/seed/2/500'),
-('Simas Canejo', str_to_date('2001.06.23', '%Y.%m.%d'), 'F', 'nhwxchief@ibelnsep.com', 'https://picsum.photos/seed/3/500'),
-('Romeu Parracho', str_to_date('2002.08.05', '%Y.%m.%d'), 'F', 'semilando@oanhxintv.com', 'https://picsum.photos/seed/4/500'),
-('Quinaz Carvalheiro', str_to_date('2000.02.15', '%Y.%m.%d'), 'M', 'smahi@ebarg.net', 'https://picsum.photos/seed/5/500'),
-('Naiara Moura', str_to_date('2001.05.18', '%Y.%m.%d'), 'M', 'chrisbieniek@capitalistdilemma.com', 'https://picsum.photos/seed/6/500'),
-('Temes Caetano', str_to_date('2001.03.11', '%Y.%m.%d'), 'M', 'gzoo55@ccvisal.xyz', 'https://picsum.photos/seed/7/500'),
-('Tom Paz', str_to_date('2000.09.23', '%Y.%m.%d'), 'F', 'markowael@tchoeo.com', 'https://picsum.photos/seed/8/500'),
-('Joyce Veloso', str_to_date('2002.11.03', '%Y.%m.%d'), 'F', 'lsykes@tchoeo.com', 'https://picsum.photos/seed/9/500'),
-('Cloe Boeira', str_to_date('1999.01.21', '%Y.%m.%d'), 'M', 'kinnik4@oanhxintv.com', 'https://picsum.photos/seed/10/500');
+('Garcez Milheiriço', '2002-03-05', 'F', 'cusaum@eetieg.com', 'https://picsum.photos/seed/1/500'),
+('Georgi Bogado', '2001-04-10', 'M', 'kristian4811@filevino.com', 'https://picsum.photos/seed/2/500'),
+('Simas Canejo', '2001-06-23', 'F', 'nhwxchief@ibelnsep.com', 'https://picsum.photos/seed/3/500'),
+('Romeu Parracho', '2002-08-05', 'F', 'semilando@oanhxintv.com', 'https://picsum.photos/seed/4/500'),
+('Quinaz Carvalheiro', '2000-02-15', 'M', 'smahi@ebarg.net', 'https://picsum.photos/seed/5/500'),
+('Naiara Moura', '2001-05-18', 'M', 'chrisbieniek@capitalistdilemma.com', 'https://picsum.photos/seed/6/500'),
+('Temes Caetano', '2001-03-11', 'M', 'gzoo55@ccvisal.xyz', 'https://picsum.photos/seed/7/500'),
+('Tom Paz', '2000-09-23', 'F', 'markowael@tchoeo.com', 'https://picsum.photos/seed/8/500'),
+('Joyce Veloso', '2002-11-03', 'F', 'lsykes@tchoeo.com', 'https://picsum.photos/seed/9/500'),
+('Cloe Boeira', '1999-01-21', 'M', 'kinnik4@oanhxintv.com', 'https://picsum.photos/seed/10/500');
 
 INSERT INTO Disciplina (Nome, Docente) VALUES
 ('Matemática', 'Adelaide Faustino'), 
@@ -101,8 +101,8 @@ INSERT INTO Inscricao (IDDisciplina, IDAluno, Nota) VALUES
 (5, 10, 19.01);
 
 INSERT INTO Revisao (Dia_Revisao, IDDisciplina, IDAluno, Nota_Antes, Nota_Depois, Efetivada, Fechada) VALUES
-(str_to_date('2035.03.21', '%Y.%m.%d'), 2, 3, 6.91, 11.56, 'S', 'N'),
-(str_to_date('2022.05.11', '%Y.%m.%d'), 5, 7, 8.89, 16.26, 'N', 'N'),
+('2035-03-21', 2, 3, 6.91, 11.56, 'S', 'N'),
+('2022-05-11', 5, 7, 8.89, 16.26, 'N', 'N'),
 (CURRENT_DATE, 3, 5, 2.08, 10.08, 'S', 'S'),
-(str_to_date('2008.08.27', '%Y.%m.%d'), 4, 10, 5.50, 14.85, 'S', 'N'),
-(str_to_date('2014.12.07', '%Y.%m.%d'), 1, 9, 10.87, 12.04, 'S', 'S');
+('2008-08-27', 4, 10, 5.50, 14.85, 'S', 'N'),
+('2014-12-07', 1, 9, 10.87, 12.04, 'S', 'S');
